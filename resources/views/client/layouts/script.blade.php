5918679972e2c16823a1a6dc48e45674a97a0496
nguyenduc2512 <nguyenduc25121999@gmail.com>
2019-12-03 15:00:48 +0700

    edit login

--- resources/views/client/layouts/script.blade.php
+++ resources/views/client/layouts/script.blade.php
@@ -1,11 +1,11 @@
 
-
+<script src="{{asset('client/js/jquery.min.js')}}"></script>
 <script src="{{asset('client/plugins/bootstrap/js/jquery.slim.min.js')}}"></script>
 <script src="{{asset('client/plugins/bootstrap/js/popper.min.js')}}"></script>
 <script src="{{asset('client/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
 <script src="{{asset('client/plugins/OwlCarousel2/js/owl.carousel.min.js')}}"></script>
 <script src="{{asset('client/js/compare.js')}}"></script>
-<script src="{{asset('client/js/jquery.min.js')}}"></script>
+
 <script src="{{asset('client/js/js.js')}}"></script>
 <script src="{{asset('client/js/cart.js')}}"></script>
 <script src="{{asset('client/js/wow.min.js')}}"></script>
