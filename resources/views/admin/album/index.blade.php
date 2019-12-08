9eb081289cea94264e2dbb5c2ebeb15b234d0b03
quochungit <quochung30399@gmail.com>
2019-12-09 00:08:05 +0700

    edit_9/12

--- resources/views/admin/album/index.blade.php
+++ resources/views/admin/album/index.blade.php
@@ -1,73 +1,98 @@
 @extends('admin/layouts/main')
 @section('content')
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Album sản phẩm {{$product->name}}</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
-                            <li class="breadcrumb-item active">Album Ảnh</li>
-                        </ol>
-                    </div>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Album ảnh: {{$product->name}}</h1>
                 </div>
-            </div><!-- /.container-fluid -->
-        </section>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
+                        <li class="breadcrumb-item active">Album Ảnh</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <div class="card-body">
-                    <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6"></div>
-                            <div class="col-sm-12 col-md-6"></div>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                <section class="content" style="padding: 20px">
+                    <div class="row">
+                        <div class="col-md-10">
+                            <h4>Ảnh</h4>
                         </div>
-                        <div>
-                            <div class="tab-content" id="pills-tabContent">
-                                <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-                                     aria-labelledby="pills-home-tab">
-                                    <table id="example" class="table table-striped" style="width:100%">
-                                        <thead>
-                                        <tr>
-                                            <th>Ảnh </th>
-                                            <th width="150px"> <a href="{{route('admin.new_picture', ['id' => $product->id])}}"
-                                                                  class="btn btn-xs btn-success">
-                                                    <i class="fa fa-plus"></i> Thêm mới
-                                                </a></th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach($album as $picture)
-                                            <tr>
-                                                <td><img src="/{{$picture->picture}}"></td>
-                                                <td>
-                                                    <a href="{{route('admin.remove_picture', ['id' => $picture->id])}}" class="btn btn-xs btn-danger btn-remove">
-                                                        <i class="fa fa-trash"></i> Xoá
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                    <form action="{{route('admin.new_picture', ['id' => $product->id])}}" method="post" enctype="multipart/form-data">
-                                        @csrf
-                                        <input type="file" name="picture[]" multiple>
-                                        <button type="submit">Thêm ảnh</button>
-                                    </form>
+                        <div class="col-md-2">
+                            <h5>
+                                <a href="{{route('admin.new_picture', ['id' => $product->id])}}"
+                                    class="btn btn-xs btn-success">
+                                    <i class="fa fa-plus"></i> Thêm mới
+                                </a>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        @foreach($album as $picture)
+                        <div class="col-md-2">
+                            <div class="img_album">
+                                <img width="100%" src="/{{$picture->picture}}">
+                                <div class="del_img">
+                                    <a href="{{route('admin.remove_picture', ['id' => $picture->id])}}">
+                                        X
+                                    </a>
                                 </div>
                             </div>
                         </div>
+                        @endforeach
                     </div>
-                </div>
+                    <div style="margin-top: 50px">
+                        <h5>Thêm ảnh mới</h5>
+                        <form action="{{route('admin.new_picture', ['id' => $product->id])}}" method="post"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <input type="file" name="picture[]" multiple>
+                            <button type="submit">Thêm ảnh</button>
+                        </form>
+                    </div>
+                </section>
+
             </div>
-        </section>
-        <!-- /.content -->
-    </div>
+        </div>
+
+    </section>
+    <!-- /.content -->
+
+</div>
 
 
 @endsection
+
+
+<style>
+.img_album img {
+    object-fit: cover;
+    height: 150px;
+}
+
+.del_img a {
+    color: white;
+    position: absolute;
+    top: 0px;
+    right: 8px;
+    background: red;
+    width: 25px;
+    height: 25px;
+    text-align: center;
+}
+
+.del_img a:hover {
+    background: #000;
+    color: white;
+}
+</style>
--- resources/views/client/cate.blade.php
+++ resources/views/client/cate.blade.php
@@ -1,53 +1,55 @@
 @extends('client/layouts/main')
 @section('content')
-   <div class="path_link">
-        <div class="container">
-            <a href="index.html">Trang chủ</a> > <span>Tên danh mục</span>
-        </div>
+<div class="path_link">
+    <div class="container">
+        <a href="index.html">Trang chủ</a> > <span>Tên danh mục</span>
     </div>
+</div>
 
-    <div class="cate">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="fitter">
-                        <div class="row">
-                            <div class="col-lg-12 col-6">
-                                <h3>Tên dang mục</h3>
-                                @foreach($category as $cate)
-                                <div class="col-lg-12">
-                                    <a href="{{route('cate', ['id' => $cate->id])}}"> {{$cate->name}} <span>({{$cate->products_count}})</span></a>
-                                </div>
-                                @endforeach
+<div class="cate">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="fitter">
+                    <div class="row">
+                        <div class="col-lg-12 col-6">
+                            <h3>Tên dang mục</h3>
+                            @foreach($category as $cate)
+                            <div class="col-lg-12">
+                                <a href="{{route('cate', ['id' => $cate->id])}}"> {{$cate->name}}
+                                    <span>({{$cate->products_count}})</span></a>
                             </div>
-                            <div class="col-lg-12 col-6">
-                                <h3 style="margin-top: 20px;">Thương hiệu</h3>
-                                    @foreach(App\Models\Brand::all() as $brand)
-                                    <div class="col-lg-12 mt-2"> 
-                                        <input type="checkbox" value="{{$brand->id}}" id="brand"  class="find brand"><span>{{$brand->name}}</span>
-                                    </div>
-                                    @endforeach
+                            @endforeach
+                        </div>
+                        <div class="col-lg-12 col-6">
+                            <h3 style="margin-top: 20px;">Thương hiệu</h3>
+                            @foreach(App\Models\Brand::all() as $brand)
+                            <div class="col-lg-12 mt-2">
+                                <input type="checkbox" value="{{$brand->id}}" id="brand"
+                                    class="find brand"><span>{{$brand->name}}</span>
                             </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
-                <div class="col-md-9">
-                    <div class="sortby">
-                        <select name="" id="seID" class="find">
-                            <option value="">Sắp xếp theo</option>
-                            <option value="new">Mới nhất</option>
-                            <option value="asc">Giá: Thấp - Cao</option>
-                            <option value="desc">Giá: Cao - Thấp</option>
-                        </select>
-                        <input type="hidden" value="{{$id}}" name="" id="cateID">
-                    </div>
-                    <div class="pcate">
-                        <div class="row" id="productCate">
-                            @include('client/procate')
-                        </div>
+            </div>
+            <div class="col-md-9">
+                <div class="sortby">
+                    <select name="" id="seID" class="find">
+                        <option value="">Sắp xếp theo</option>
+                        <option value="new">Mới nhất</option>
+                        <option value="asc">Giá: Thấp - Cao</option>
+                        <option value="desc">Giá: Cao - Thấp</option>
+                    </select>
+                    <input type="hidden" value="{{$id}}" name="" id="cateID">
+                </div>
+                <div class="pcate">
+                    <div class="row" id="productCate">
+                        @include('client/procate')
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
